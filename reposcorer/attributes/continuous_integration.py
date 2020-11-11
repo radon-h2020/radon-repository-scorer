a560f2c7efa480d93735546275808dc9a25e9d1f
Stefano Dalla Palma <stefano.dallapalma0@gmail.com>
2020-11-11 14:57:00 +0100

    Added Jenkinsfile, .ci, .circle, .github/workflows to continuous integration services

--- reposcorer/attributes/continuous_integration.py
+++ reposcorer/attributes/continuous_integration.py
@@ -2,9 +2,10 @@
 The metric for the continuous integration dimension may be defined as a piecewise \
 function that returns 1 if the repo has evidence of Continuous Integrations, \
 identified by the presence of Travis CI, Hound, Appveyor, Shippable, MagnumCI, \
-Solano, CircleCI, and Wercker files.
+Solano, CircleCI, Wercker, and Jenkinsfile files.
+If none is present, check if .github/workflows/ exists and is not empty
 """
-from os import listdir
+import os
 
 
 def has_continuous_integration(path_to_repo: str) -> bool:
@@ -12,8 +13,13 @@ def has_continuous_integration(path_to_repo: str) -> bool:
     :param path_to_repo: the path to the repository to analyze
     :return: True if the repository contains evidence of a continuous integration practice. False otherwise
     """
-    ci_services = {'.travis.yml', '.hound.yml', 'appveyor.yml', '.magnum.yml', 'circle.yml', 'shippable.yml',
-                   'solano.yml',
-                   'wercker.yml'}
+    ci_services = {'.travis.yml', '.hound.yml', 'appveyor.yml', '.magnum.yml', 'circle.yml', '.ci', '.circle',
+                   'shippable.yml', 'solano.yml', 'wercker.yml', 'Jenkinsfile'}
 
-    return any(service in listdir(path_to_repo) for service in ci_services)
+    has_ci = any(service in os.listdir(path_to_repo) for service in ci_services)
+
+    if not has_ci:
+        github_workflow_dir = os.path.join(path_to_repo, '.github', 'workflows')
+        has_ci = os.path.isdir(github_workflow_dir) and len(os.listdir(github_workflow_dir))
+
+    return has_ci
