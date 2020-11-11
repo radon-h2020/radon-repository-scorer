38f04039676c10f9be319a74889deab56ff57fab
Stefano Dalla Palma <stefano.dallapalma0@gmail.com>
2020-11-11 14:58:36 +0100

    Added licence (in additon to license) to licensing

--- reposcorer/attributes/licensing.py
+++ reposcorer/attributes/licensing.py
@@ -5,5 +5,6 @@ that returns 1 if the repository has a license, 0 otherwise
 
 import os
 
-def has_license(path_to_repo:str) -> bool:
-    return any('license' in name.lower() for name in os.listdir(path_to_repo))
+
+def has_license(path_to_repo: str) -> bool:
+    return any('license' in name.lower() or 'licence' in name.lower() for name in os.listdir(path_to_repo))
